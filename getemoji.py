eef789cc0315d7094ba396943fd10227aad964bd
Sonny Kim <sonny@fogcreek.com>
2015-07-28 18:10:02 -0400

    proof of concept

--- getemoji.py
+++ getemoji.py
@@ -3,11 +3,10 @@ import urllib
 import cStringIO
 import json
 from PIL import Image
-from pyquery import PyQuery as pq
+from io import BytesIO
 
-#url = 'https://www.google.com/search?hl=en&site=imghp&tbm=isch&source=hp&q='
 url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q='
-query = 'full house poker'
+query = 'fog creek software'
 query_url = url + urllib.quote_plus(query)
 print query_url
 user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36'
@@ -16,7 +15,15 @@ results = r.json()
 
 for x in results['responseData']['results']:
     file = cStringIO.StringIO(urllib.urlopen(x['url']).read())
-    img = Image.open(file)
-    img.show()
-    print x['url']
+    try:
+        img = Image.open(file)
+        (i_width, i_height) = img.size
+        img.thumbnail((128, 128), Image.ANTIALIAS)
+        img_file = BytesIO()
+        img.save(img_file, 'png')
+        img_size = img_file.tell()
+        img.show()
+        print 'url: %s size: %s orig_width: %s orig_height: %s' % (x['url'], img_size, i_width, i_height)
+    except:
+        pass
     
